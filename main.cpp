058128e179eabda2a25e07d4d6468e40c3c14378
Maciej Stępień <maciek1284@outlook.com>
2017-05-09 20:48:06 +0200

    dobór porogów

--- main.cpp
+++ main.cpp
@@ -9,24 +9,27 @@ using namespace std;
 int main()
 {
     VideoCapture capture = VideoCapture(1);
-    Mat frame;
-    Mat hsv_img;
+    string window_name [] = { "Kamera", "Binary", "Blur" , "Erode" };
+    Mat frame, img, hsv_img, binary, imgblur, imgerode;
     vector<Mat> hsv_split;
-    namedWindow("Kamerka", CV_WINDOW_AUTOSIZE);
-    namedWindow("Hsv", CV_WINDOW_AUTOSIZE);
-    namedWindow("Hue", CV_WINDOW_AUTOSIZE);
-    namedWindow("Saturation", CV_WINDOW_AUTOSIZE);
-    namedWindow("Value", CV_WINDOW_AUTOSIZE);
+    for ( int i = 0; i < 4; i++ ) namedWindow(window_name[i], CV_WINDOW_AUTOSIZE);
+    int lowerb = 0, upperb = 0;
+    createTrackbar( "lb", window_name[1], &lowerb, 255, NULL );
+    createTrackbar( "ub", window_name[1], &upperb, 255, NULL );
     while ( waitKey(20) != 27 )
     {
         capture >> frame;
-        cvtColor(frame, hsv_img, CV_BGR2HSV);
+        frame.copyTo(img);
+        cvtColor(img, hsv_img, CV_BGR2HSV);
         split(hsv_img, hsv_split);
-        imshow("Kamerka", frame);
-        imshow("Hsv", hsv_img);
-        imshow("Hue", hsv_split[0]);
-        imshow("Saturation", hsv_split[1]);
-        imshow("Value", hsv_split[2]);
+        inRange(hsv_split[0], lowerb, upperb, binary);
+        cv::Mat element(3,3,CV_8U,cv::Scalar(1));
+        imshow(window_name[1], binary);
+        blur(binary, imgblur, cv::Size(3,3) );
+        imshow(window_name[2], imgblur);
+        erode(imgblur, imgerode, element,Point(-1,-1), 2);
+        imshow(window_name[0], img );
+        imshow(window_name[3], imgerode);
     }
     capture.release();
     return 0;
