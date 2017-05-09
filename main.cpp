1e6cd7c9101adf732b9e4532c8c25ca7d6cd2576
Maciej Stępień <maciek1284@outlook.com>
2017-05-09 20:59:46 +0200

    obtracanie obrazka

--- main.cpp
+++ main.cpp
@@ -12,17 +12,21 @@ int main()
     string window_name [] = { "Kamera", "Binary", "Blur" , "Erode" };
     Mat frame, img, hsv_img, binary, imgblur, imgerode;
     vector<Mat> hsv_split;
-    for ( int i = 0; i < 4; i++ ) namedWindow(window_name[i], CV_WINDOW_AUTOSIZE);
-    int lowerb = 0, upperb = 0;
+    for ( int i = 0; i < 3; i++ ) namedWindow(window_name[i], CV_WINDOW_AUTOSIZE);
+    int lowerb = 255, upperb = 255;
     createTrackbar( "lb", window_name[1], &lowerb, 255, NULL );
     createTrackbar( "ub", window_name[1], &upperb, 255, NULL );
     while ( waitKey(20) != 27 )
     {
         capture >> frame;
-        frame.copyTo(img);
+        //frame.copyTo(img);
+        flip(frame, img, 1);
         cvtColor(img, hsv_img, CV_BGR2HSV);
         split(hsv_img, hsv_split);
-        inRange(hsv_split[0], lowerb, upperb, binary);
+        /*imshow("Kamera", hsv_split[0]);
+        imshow("Binary", hsv_split[1]);
+        imshow("Blur", hsv_split[2]);*/
+        inRange(hsv_split[2], lowerb, upperb, binary);
         cv::Mat element(3,3,CV_8U,cv::Scalar(1));
         imshow(window_name[1], binary);
         blur(binary, imgblur, cv::Size(3,3) );
